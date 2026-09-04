c0f3fb86eba8c850b157c76a01a72a3028394956
REYAN <107745747+REYAN-YADAV@users.noreply.github.com>
2026-09-04 11:42:07 +0530

    Add files via upload

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,11 @@
 # write a python program which accept a word and find it is palindrome or not>
 
 word = input("enter word: ")
+"""
 pal = "palindrome" if word.lower() == word[::-1].lower() else "not palindrome"
 print("'{}' is {}".format(word,pal))
+"""
+if word.lower() == word[::-1].lower():
+    print("'{}' is a palindrome".format(word))
+else:
+    print("'{}' is not a palindrome".format(word))
