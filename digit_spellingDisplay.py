df000a57a34da08fb70291d71671afdc119349db
REYAN <107745747+REYAN-YADAV@users.noreply.github.com>
2026-09-04 11:41:23 +0530

    Add files via upload

---
+++ digit_spellingDisplay.py
@@ -0,0 +1,42 @@
+# write a python program which will accept any digit and display any digit
+d = int(input("Enter the digit: "))
+if d == 0:
+    print("ZERO")
+elif d == 1:
+    print("ONE")
+elif d==2:
+    print("TWO")
+elif d==3:
+    print("THREE")
+elif d==4:
+    print("FOUR")
+elif d==5:
+    print("FIVE")
+elif d==6:
+    print("SIX")
+elif d==7:
+    print("SEVEN")
+elif d==8:
+    print("EIGHT")
+elif d==9:
+    print("NINE")
+elif d == -1:
+    print("Minus ONE")
+elif d== -2:
+    print("Minus TWO")
+elif d== -3:
+    print("Minus THREE")
+elif d== -4:
+    print("Minus FOUR")
+elif d== -5:
+    print("Minus FIVE")
+elif d== -6:
+    print("Minus SIX")
+elif d== -7:
+    print("Minus SEVEN")
+elif d== -8:
+    print("Minus EIGHT")
+elif d== -9:
+    print("Minus NINE")
+else:
+    print(" only single digit is allowed")
