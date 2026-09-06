3757977c850609db23e02d22f9df71516974898b
REYAN <107745747+REYAN-YADAV@users.noreply.github.com>
2026-09-06 17:34:40 +0530

    Add files via upload

---
+++ Accept_list_OfName_sort_ascendingANDdscending.py
@@ -0,0 +1,14 @@
+# write a python program for accepting list of numerical values and sort them in both  ascending and descending order?
+lst = list()
+print("Enter list of numerical values and press @ to stop inputing? ")
+while (True):
+    value = input()
+    if value != "@":
+        lst.append(value)
+    else:
+        break
+print(lst)
+asc = sorted(lst)
+dec = sorted(lst,reverse=True)
+print("Ascending order = ", asc)
+print("Descending order =",dec)
