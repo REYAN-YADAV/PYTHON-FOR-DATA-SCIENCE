3356c61229e76e5df4a9951963abc20ea15b34f5
REYAN <107745747+REYAN-YADAV@users.noreply.github.com>
2026-09-07 18:07:37 +0530

    Add files via upload

---
+++ functionApproach3.py
@@ -0,0 +1,16 @@
+# sum of Two number
+# ApproCH 3
+"""
+input = Taken from function call
+process = done in function body
+output = display in function body
+"""
+def addop(a,b):
+    c = a+b
+    #display the result
+    print("sum({},{}) = {}".format(a,b,c))
+#main program
+a =  float(input("enter a number: "))
+b = float(input("enter another number: "))
+addop(a,b)
+
